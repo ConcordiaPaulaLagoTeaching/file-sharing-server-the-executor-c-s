59b13bc82ec51aff53e90388e852bb4980afb147
Riad-Rakan <156610477+Riad-Rakan@users.noreply.github.com>
2025-11-15 20:42:52 -0500

    Add FileNotFoundException to main method signature

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -1,13 +1,14 @@
 package ca.concordia;
 
+import java.io.FileNotFoundException;
+
 import ca.concordia.server.FileServer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.printf("Hello and welcome!");
 
         FileServer server = new FileServer(12345, "filesystem.dat", 10 * 128);
-        // Start the file server
         server.start();
     }
 }
